e5c25a8be67db95edfed1ad5622c667ecd9f88e0
Pranav952 <pranavb952@gmail.com>
2024-03-25 11:32:23 +0545

    "Modified"

--- RockSissorPapperGame.py
+++ RockSissorPapperGame.py
@@ -33,17 +33,17 @@ image = [rock, paper, scissors]
 print("what do you chose?Type 0 for rock,1 for papper or 2 for scissor")
 user_choice = int(input("Enter your choice"))
 computer_choice = random.randint(0, 2)
-if (user_choice == computer_choice):
+if (user_choice==computer_choice):
     print(f"your choice {user_choice}:\n{image[user_choice]}")
-    print(f"computer choice {computer_choice}:\n{image[computer_choice]}")
+    print(f"computer choice:\n{image[computer_choice]}")
     print("Draw,Try again")
-elif (user_choice > computer_choice):
+elif (user_choice-computer_choice==1):
     print(f"your choice {user_choice}:\n{image[user_choice]}")
-    print(f"computer choice {computer_choice}:\n{image[computer_choice]}")
+    print(f"computer choice:\n{image[computer_choice]}")
     print("You win")
-elif (user_choice < computer_choice):
+elif (user_choice - computer_choice!=0):
     print(f"your choice {user_choice}:\n{image[user_choice]}")
-    print(f"computer choice {computer_choice}:\n{image[computer_choice]}")
+    print(f"computer choice:\n{image[computer_choice]}")
     print("You lose")
 else:
     print("Wrong choice,chose again")
