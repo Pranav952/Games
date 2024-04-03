656d2f1c80ba2f678a13472a65666978af6f8eeb
Pranav952 <pranavb952@gmail.com>
2024-04-03 13:54:42 +0545

    "ShapeGenerator"

---
+++ ShapeGenerator.py
@@ -0,0 +1,20 @@
+import random
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+turtle = Turtle()
+colors = ["lime", "magenta", "darkgreen", "gold"]
+
+
+def draw_shape(side):
+    for i in range(side):
+        angle = 360 / side
+        color = random.choice(colors)
+        turtle.color(color)
+        turtle.forward(100)
+        turtle.left(angle)
+
+
+draw_shape(4)
+screen.mainloop()
